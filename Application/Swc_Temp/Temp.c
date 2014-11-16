ee7ca77e43d4c9a47b3bb911f81ea8a1e5437c1c
Mike Reyes Ch <reyesjr88@users.noreply.github.com>
2014-11-16 14:43:16 -0600

    Added new os task functions

--- Application/Swc_Temp/Temp.c
+++ Application/Swc_Temp/Temp.c
@@ -7,15 +7,16 @@
 
 #include "Temp.h"
 
+uint32_t gTemperatureValue;
+
 uint32_t Temp_Read(void)
 {
 	uint32_t temperature;
 	uint32_t adcValue;
 
 	adcValue = Analog_Read();
-
-	temperature = (M1 - (adcValue - VTEMP25_ADC) * M2)/K;
-
+	
+	temperature = (M1 - (adcValue - VTEMP25_ADC) * 583)/K;
 
 	return temperature;
 }

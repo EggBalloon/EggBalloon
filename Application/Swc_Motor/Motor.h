b3d1b1a7a74f4c778fed7c3eb272ca085fdcc709
juanezteban <juanezteban@users.noreply.github.com>
2014-11-22 16:54:05 -0600

    Logical behavior added to APIS for FAN Init, DeInit, SetDirection, GetDirection, SetSpeed and GetSpeed

--- Application/Swc_Motor/Motor.h
+++ Application/Swc_Motor/Motor.h
@@ -9,17 +9,21 @@
 #define MOTOR_H_
 
 #include "IO_Interface.h"
+#include "Pwm_Types.h"
+#include "Motor_Types.h"
+#include "Motor_Cfg.h"
+#include "mqx.h"
 
 void FAN_InitMotorCntrl(void);
 
 void FAN_DeInitMotorCntrl(void);
 
-void FAN_SetMotorDirection(void);
+void FAN_SetMotorDirection(tenMotorID MotorID, tenMotorDir Direction, uint16_t u16Speed);
 
-void FAN_GetMotorDirection(void);
+tenMotorDir FAN_GetMotorDirection(tenMotorID MotorID);
 
-void FAN_MotorSpeed(void);
+void FAN_SetMotorSpeed(tenMotorID MotorID, uint16_t u16Speed);
 
-void FAN_GetMotorSpeed(void);
+uint16_t FAN_GetMotorSpeed(tenMotorID MotorID);
 
 #endif /* MOTOR_H_ */

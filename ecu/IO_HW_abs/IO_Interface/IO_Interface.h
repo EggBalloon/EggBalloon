92531d23f78bbfe0e9c0dd06be1a13337f4eeacb
Mike Reyes Ch <reyesjr88@users.noreply.github.com>
2014-11-22 16:10:31 -0600

    Defines for RGB pins were not set properly. So, defines were adjusted to match pins green, red and blue.

--- ecu/IO_HW_abs/IO_Interface/IO_Interface.h
+++ ecu/IO_HW_abs/IO_Interface/IO_Interface.h
@@ -24,38 +24,10 @@ typedef enum
 	enPWM5,
 }Pwm_tenPwm;
 
-#define DIO0	(0x0100)
-#define DIO1	(0x0200)
-#define DIO2	(0x0300)
-#define DIO3	(0x0400)
-#define DIO4	(0x0500)
-#define DIO5	(0x0600)
-#define DIO6	(0x0700)
-#define DIO7	(0x0800)
-#define DIO8	(0x0900)
-#define DIO9	(0x0A00)
-#define DIO10 	(0x0B00)
-#define DIO11 	(0x0C00)
-#define DIO12 	(0x0D00)
-#define DIO13 	(0x0E00)
-#define DIO14 	(0x0F00)
-#define DIO15 	(0x1000)
-#define DIO16	(0x1100)
-#define DIO17	(0x1200)
-#define DIO18	(0x1300)
-#define DIO19	(0x1400)
-#define DIO20	(0x1500)
-#define DIO21	(0x1600)
-#define DIO22	(0x1700)
-#define DIO23	(0x1800)
-#define DIO24	(0x1900)
-#define DIO25	(0x2000)
-#define DIO26 	(0x2100)
-#define DIO27 	(0x2200)
-#define DIO28 	(0x2300)
-#define DIO29 	(0x2400)
-#define DIO30 	(0x2500)
-#define DIO31 	(0x2600)
+#define DIO21	(0x1500)
+#define DIO22	(0x1600)
+#define DIO26 	(0x1A00)
+
 
 uint32_t Analog_Read(void);
 
